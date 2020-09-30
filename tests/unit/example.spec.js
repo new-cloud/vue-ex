2034235a944687f12d84566915ab9a00d6449c48
zh_New <739493331@qq.com>
2020-09-30 15:40:31 +0800

    ''

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -3,7 +3,6 @@ import { shallowMount } from '@vue/test-utils';
 import HelloWorld from '@/components/HelloWorld.vue';
 
 describe('HelloWorld.vue', () => {
-	// 参数if是自己乱加的
 	it('renders props.msg when passed', () => {
 		const msg = 'new message';
 		const wrapper = shallowMount(HelloWorld, {
