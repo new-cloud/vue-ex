cf2674c1a1012bf82f7c30cd96ef3187d9d93ee1
zh_New <739493331@qq.com>
2020-11-16 14:57:56 +0800

    utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,11 @@
 
-function adds() {
-	console.log(222);
+// 工具库
+
+export function numConvert(num){
+	if(num > 10000){
+		num = (num/10000).toFixed(2)+'W';
+	}else if(num > 1000){
+		num = (num/1000).toFixed(2)+'K';
+	}
+	return num;
 }
-export default {
-	name: 'xx',
-	fu: adds
-};
