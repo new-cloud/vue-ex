261fa06ac20fbd662618cecd47b54053d19d5837
zh_New <739493331@qq.com>
2020-11-18 10:00:46 +0800

    layout

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,7 @@
 <template>
   <div id="app">
-    <router-view/>
+    <header></header>
+    <router-view class="app-content" />
+    <footer></footer>
   </div>
 </template>
-
-<style lang="scss" scoped>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-</style>
